f5377e5338c47ced725c15ce5aa628b70f14ef33
Wouter <wstraver@newnexus.nl>
2022-01-30 15:46:15 +0100

    Initial Commit

---
+++ Shared/SwiftUIUIChallengesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIUIChallengesApp.swift
+//  Shared
+//
+//  Created by Wouter Straver on 30/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIUIChallengesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
