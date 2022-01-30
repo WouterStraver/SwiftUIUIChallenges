d8789ba9f2c213c3cadb95d50c283a647dcf1f55
Wouter <wstraver@newnexus.nl>
2022-01-30 15:51:18 +0100

    Added basics

---
+++ Shared/DayOneView.swift
@@ -0,0 +1,20 @@
+//
+//  DayOneView.swift
+//  SwiftUIUIChallenges
+//
+//  Created by Wouter Straver on 30/01/2022.
+//
+
+import SwiftUI
+
+struct DayOneView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DayOneView_Previews: PreviewProvider {
+    static var previews: some View {
+        DayOneView()
+    }
+}
